8ef568f8ebec2ff38b641adaa899d479516d213a
jeromealapad13 <58656012+jeromealapad13@users.noreply.github.com>
2020-03-22 15:58:34 +0800

    removed IDs

--- weaponList.py
+++ weaponList.py
@@ -14,7 +14,6 @@ class shortSword(Weapon):
         super().__init__("Short Sword", 
                         "A short sword suitable for beginner knights.", 
                         "KNIGHT", 5, rdi(2, 3))
-        self.ID = "1x00001"
 
 class SwordAndSheild(Weapon):
     def __init__(self):
@@ -25,7 +24,6 @@ class TwoHandedSword(Weapon):
     def __init__(self):
         super().__init__("Two Handed Sword",
                         "Heavy Sword that deals great damage.")
-        self.ID = "1x00002"
 
 """ Barbarian Weapon """
 
@@ -34,19 +32,16 @@ class Axe(Weapon):
         super().__init__("Axe",
                         "Heavy Blade that can cut through anything.",
                         "")
-        self.ID = "1x00003"
 
 """ Mage Weapon """
 
 class staff(Weapon):
     def __init__(self):
         super().__init__("Staff")
-        self.ID = "1x00004"
 
 class tome(Weapon):
     def __init(self):
         super().__init__("Tome")
-        self.ID = "1x00005"
 
 """ Archer Weapons """
 
@@ -54,16 +49,13 @@ class shortBow(Weapon):
     def __init__(self):
         super().__init__("Short Bow",
                         "Bow used by novice archers.")
-        self.ID = "1x00006"
 
 class longBow(Weapon):
     def __init__(self):
         super().__init__("Long Bow",
                         "Who are you? Legolas?.")
-        self.ID = "1x00007"
 
 class crossBow(Weapon):
     def __init__(self):
         super().__init__("Crossbow",
                         "A bow for cheeky bastards.")
-        self.ID = "1x00008"
